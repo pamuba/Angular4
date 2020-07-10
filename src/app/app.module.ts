f8ffca91eed9c99137f8faae8f0a33dba177a783
pamuba <pamuba@users.noreply.github.com>
2020-07-10 21:30:14 +0530

    16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BlueColorDirective } from './blue-color.directive';
 import { HttpClientModule } from '@angular/common/http'
@@ -11,7 +11,18 @@ import { Emp1Component } from './emp1.component'
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { LoginFormComponent } from './login-form/login-form.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { RouterModule, Routes } from '@angular/router';
+import { UserService } from './user.service';
+import { AuthguardGuard } from './authguard.guard';
+
+
+const appRoutes:Routes=[
+  {path:"", component:LoginFormComponent},
+  {path:"dashboard", canActivate:[AuthguardGuard],  component:DashboardComponent}
+]
+
 
 @NgModule({
   declarations: [
@@ -23,14 +34,17 @@ import { FooterComponent } from './footer/footer.component'
     Emp1Component,
     HeaderComponent,
     LoginFormComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
-  providers:[EmployeeService],
+  providers:[EmployeeService, UserService, AuthguardGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'app/user.service';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private user:UserService) { }
 
   ngOnInit() {
+  }
+  loginUser(e){
+    console.log(e)
+    var username = e.target.elements[0].value;
+    var password = e.target.elements[1].value;
+    // console.log(username, password);
+    // return false;
+    if(username == 'admin' && password == 'admin'){
+      this.user.setUserLoggedIn();
+      this.router.navigate(['dashboard'])
+    }
   }
 
 }
